21b27cf5c74fc90ba2b3adfefb0db8981c5f1b33
Pankaj kumar <2023251023.pankaj@ug.sharda.ac.in>
2026-08-11 03:01:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (79.78%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int s = 0;
+
+            while (n != 0) {
+                int digit = n % 10;
+                s += digit * digit;
+                n /= 10;
+            }
+
+            n = s;
+        }
+
+        return n == 1;
+    }
+}
